90881c8f720216f4563fba9bf7bf4417ede7ba71
Muhammadkarim <uzbekdev1998@gmail.com>
2024-01-24 15:01:59 +0500

    added ts file

---
+++ index.js
@@ -0,0 +1,10 @@
+function firstEl(arr) {
+    return arr[0];
+}
+var number = [1, 2, 3, 4];
+var fn1 = firstEl(number);
+console.log(fn1);
+var str = ["row", 890];
+var fn2 = firstEl(str);
+console.log(fn2);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,10 @@
+function firstEl(arr: any[]) {
+  return arr[0];
+}
+const number = [1, 2, 3, 4];
+const fn1 = firstEl(number);
+console.log(fn1);
+
+const str = ["row", 890];
+const fn2 = firstEl(str);
+console.log(fn2);
