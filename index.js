8b05fe51132088834e9a71a9d2e3da0cf7a1b7c7
Muhammadkarim <uzbekdev1998@gmail.com>
2024-01-26 14:58:36 +0500

    Added TS types

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// SIMPLE FN IN TYPESCRIPT
 function firstEl(arr) {
     return arr[0];
 }
@@ -7,4 +8,39 @@ console.log(fn1);
 var str = ["row", 890];
 var fn2 = firstEl(str);
 console.log(fn2);
+// SIMPLE object IN TYPESCRIPT
+var obj;
+obj = { name: "John", age: 26, surname: "Sam" };
+console.log(obj);
+// FUNCTION IN TS
+function add(a, b) {
+    return a + b;
+}
+add(add(10, 10), 10);
+// OPTIONAL PROPERTIES
+function printCar(car) {
+    var str = "About my car: ".concat(car.model, ", ").concat(car.year, ", ").concat(car.year);
+    car.voltage;
+    if (typeof car.voltage !== "undefined") {
+        str += " ".concat(car.voltage, "v");
+    }
+    return str;
+}
+// THIS CODE GIVES ERROR BECAUSE 'color' does not exist in type!
+console.log(printCar({ model: "Tesla Y", year: 2005, make: "Tesla", voltage: 220, color: "red" }));
+// THIS CODE WORKS BECAUSE 'COLOR' VALUE HOLDEN INTO ANOTHER VARIABLE
+var superCar = { model: "Tesla Y", year: 2005, make: "Tesla", voltage: 220, color: "red" };
+console.log(printCar(superCar));
+// ARRAY
+var object = { item1: [], item2: [] };
+object = { item1: [89, 1], item2: [9010, 100] };
+console.log(object);
+// TUPLE
+var myCar;
+myCar = [100, 34, "oops"]; // Type 'number' is not assignable to type 'string'.
+myCar = ["oops", 34, 21, 23]; //  Source has 4 element(s) but target allows only 3. (LIMITATION)
+// UNION TYPE
+var value;
+value = 77; // OK
+value = ["work"]; // OK
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// SIMPLE FN IN TYPESCRIPT
 function firstEl(arr: any[]) {
   return arr[0];
 }
@@ -8,3 +9,47 @@ console.log(fn1);
 const str = ["row", 890];
 const fn2 = firstEl(str);
 console.log(fn2);
+
+// SIMPLE object IN TYPESCRIPT
+let obj: { name: string; age: number; surname: string };
+obj = { name: "John", age: 26, surname: "Sam" };
+console.log(obj);
+
+// FUNCTION IN TS
+function add(a: number, b: number): number {
+  return a + b;
+}
+add(add(10, 10), 10);
+
+// OPTIONAL PROPERTIES
+function printCar(car: { model: string; year: number; make: string; voltage?: number }) {
+  let str = `About my car: ${car.model}, ${car.year}, ${car.year}`;
+  car.voltage;
+
+  if (typeof car.voltage !== "undefined") {
+    str += ` ${car.voltage}v`;
+  }
+  return str;
+}
+
+// THIS CODE GIVES ERROR BECAUSE 'color' does not exist in type!
+console.log(printCar({ model: "Tesla Y", year: 2005, make: "Tesla", voltage: 220, color: "red" }));
+
+// THIS CODE WORKS BECAUSE 'COLOR' VALUE HOLDEN INTO ANOTHER VARIABLE
+const superCar = { model: "Tesla Y", year: 2005, make: "Tesla", voltage: 220, color: "red" };
+console.log(printCar(superCar));
+
+// ARRAY
+let object = { item1: [], item2: [] };
+object = { item1: [89, 1], item2: [9010, 100] };
+console.log(object);
+
+// TUPLE
+let myCar: [string, number, number];
+myCar = [100, 34, "oops"]; // Type 'number' is not assignable to type 'string'.
+myCar = ["oops", 34, 21, 23]; //  Source has 4 element(s) but target allows only 3. (LIMITATION)
+
+// UNION TYPE
+let value: number | string[];
+value = 77; // OK
+value = ["work"]; // OK
